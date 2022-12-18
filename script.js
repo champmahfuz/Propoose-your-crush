ae3c3a7b1ea409a1da9cb3b3faf22939a8c2fe42
Mahfuz Rahman <mahfuzrahman517@gmail.com>
2022-12-18 20:47:57 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yes-btn').addEventListener('click', function () {
+    document.getElementById('msg').innerText = 'I Love You';
+});
+
+function proposeSender() {
+    document.getElementById('msg').innerText = 'I Love You Very Much';
+}
